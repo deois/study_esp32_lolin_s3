ccf13abd8a17471c9ab8c6f05edda5ef6b155cf0
happyhajo01 <happyhajo@naver.com>
2022-11-30 08:28:39 +0900

    실패, 보드 죽음

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import time
 
 import machine
 
+import threading
+
 machine.freq()  # get the current frequency of the CPU
 machine.freq(240000000)  # set the CPU frequency to 240 MHz
 
@@ -12,25 +14,78 @@ neo = NeoPixel(Pin(38, Pin.OUT), 1)
 neo.ORDER = (0, 1, 2, 3)
 RGB = [100, 0, 0]
 # pinLED = Pin(38, Pin.OUT)  # create output pin on GPIO0
+type_pin = Pin(5, Pin.IN)
 
-while True:
-    RGB = [100, 0, 0]
-    neo[0] = tuple(RGB)
-    neo.write()
-    print(RGB)
-    time.sleep(1)
+def start_led():
+    t = threading.Timer(0.1,change_led)
+    t.deamon = True
+    t.start()
 
-    RGB = [0, 100, 0]
-    neo[0] = tuple(RGB)
-    neo.write()
-    print(RGB)
-    time.sleep(1)
+def change_led():
+    while True:
+        if type_pin.value() != 1 :
+            basic_led()
+        else :
+            gradation()
 
-    RGB = [0, 0, 100]
-    neo[0] = tuple(RGB)
-    neo.write()
-    print(RGB)
-    time.sleep(1)
+def basic_led():
+    while True:
+        RGB = [100, 0, 0]
+        neo[0] = tuple(RGB)
+        neo.write()
+        print(RGB)
+        time.sleep(1)
+
+        RGB = [0, 100, 0]
+        neo[0] = tuple(RGB)
+        neo.write()
+        print(RGB)
+        time.sleep(1)
+
+        RGB = [0, 0, 100]
+        neo[0] = tuple(RGB)
+        neo.write()
+        print(RGB)
+        time.sleep(1)
+
+def gradation():
+    while True:
+        for r in (0,257):
+            RGB = [r, 0, 0]
+            neo[0] = tuple(RGB)
+            neo.write()
+            print(RGB)
+            time.sleep(0.1)
+            for g in (0,257):
+                RGB = [r, g, 0]
+                neo[0] = tuple(RGB)
+                neo.write()
+                print(RGB)
+                time.sleep(0.1)
+                for b in (0,257):
+                    RGB = [r, g, b]
+                    neo[0] = tuple(RGB)
+                    neo.write()
+                    print(RGB)
+                    time.sleep(0.1)
+                    for b in (257, 0, -1):
+                        RGB = [r, g, b]
+                        neo[0] = tuple(RGB)
+                        neo.write()
+                        print(RGB)
+                        time.sleep(0.1)
+                        for g in (257, 0, -1):
+                            RGB = [r, g, b]
+                            neo[0] = tuple(RGB)
+                            neo.write()
+                            print(RGB)
+                            time.sleep(0.1)
+                            for r in (257, 0, -1):
+                                RGB = [r, g, b]
+                                neo[0] = tuple(RGB)
+                                neo.write()
+                                print(RGB)
+                                time.sleep(0.1)
 
 import network
 
@@ -46,3 +101,8 @@ import network
 # ap.config(ssid='ESP-AP') # set the SSID of the access point
 # ap.config(max_clients=10) # set how many clients can connect to the network
 # ap.active(True)         # activate the interface
+
+if __name__ == '__main__':
+    # change_led()
+    # start_led()
+    gradation()
